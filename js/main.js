90d3f5f87c562ea48eb62b0cf79246a9b3d355a8
Alish Gosai <hellow6190@gmail.com>
2024-06-17 23:20:10 +1000

    testing some craziess

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   navToggle.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+    navToggle.classList.toggle('active');
   });
 
   // Close nav-links when clicking outside
   document.addEventListener('click', (e) => {
     if (!navLinks.contains(e.target) && !navToggle.contains(e.target)) {
       navLinks.classList.remove('show');
+      navToggle.classList.remove('active');
     }
   });
 
@@ -17,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', () => {
     if (window.innerWidth > 768) {
       navLinks.classList.remove('show');
+      navToggle.classList.remove('active');
     }
   });
 
@@ -30,22 +33,33 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// for the mouse animaiton
 
+document.addEventListener("mousemove", function(event) {
+  var mouseX = event.pageX;
+  var mouseY = event.pageY;
+
+  var image = document.getElementById("alish-image");
+  var halfWidth = image.width / 2;
+  var halfHeight = image.height / 2;
+
+  image.style.left = (mouseX - halfWidth) + "px";
+  image.style.top = (mouseY - halfHeight) + "px";
+});
 
 // main.js
 document.addEventListener('scroll', function() {
   const homeSection = document.querySelector('#home .cover-image');
   const scrollPosition = window.scrollY;
-  
+
   // Adjust the speed by changing the divisor value
   homeSection.style.transform = `translateY(${scrollPosition / 1.5}px)`;
 });
 
-
 document.addEventListener('scroll', function() {
   const homeSection = document.querySelector('#home .arrow-box');
   const scrollPosition = window.scrollY;
-  
+
   // Adjust the speed by changing the divisor value
   homeSection.style.transform = `translateY(${scrollPosition / 2}px)`;
 });
