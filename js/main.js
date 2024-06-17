848180a9be4d36b2306df00cff755c224c2ab224
Alish Gosai <hellow6190@gmail.com>
2024-06-17 19:27:01 +1000

    responsiveness to my navbar and more

--- js/main.js
+++ js/main.js
@@ -42,3 +42,18 @@ document.addEventListener('scroll', function() {
 });
 
 
+document.addEventListener('scroll', function() {
+  const homeSection = document.querySelector('#home .arrow-box');
+  const scrollPosition = window.scrollY;
+  
+  // Adjust the speed by changing the divisor value
+  homeSection.style.transform = `translateY(${scrollPosition / 2}px)`;
+});
+
+// Add an event listener to the scroll-down anchor
+document.querySelector('.scroll-down').addEventListener('click', function(e) {
+  e.preventDefault();
+  const aboutPage = document.querySelector('#about');
+  const aboutPageTop = aboutPage.offsetTop;
+  window.scrollTo({ top: aboutPageTop, behavior: 'smooth' });
+});
