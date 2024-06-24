624d67f441f9af30b0c60b69e1f58ef5b731df42
Alish Gosai <hellow6190@gmail.com>
2024-06-24 11:26:02 +1000

    working out with the navbar makeing it better

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const navToggle = document.querySelector('.nav-toggle');
-  const navLinks = document.querySelector('.nav-links');
-
-  navToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-    navToggle.classList.toggle('active');
-  });
-
-  // Close nav-links when clicking outside
-  document.addEventListener('click', (e) => {
-    if (!navLinks.contains(e.target) && !navToggle.contains(e.target)) {
-      navLinks.classList.remove('show');
-      navToggle.classList.remove('active');
-    }
-  });
-
-  // Toggle navLinks on resizing window
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
-      navLinks.classList.remove('show');
-      navToggle.classList.remove('active');
-    }
-  });
-
-  document.addEventListener('scroll', () => {
-    const nav = document.querySelector('.nav');
-    if (window.scrollY > 50) {
-      nav.classList.add('scrolled');
-    } else {
-      nav.classList.remove('scrolled');
-    }
-  });
-});
 
 // for the mouse animaiton
 
